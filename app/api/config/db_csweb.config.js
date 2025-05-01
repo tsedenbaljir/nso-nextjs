1c117f091dcfc54d07aa0a3309133cc49daa8e2b
tsedenbaljir <tsedenbaljir@nso.mn>
2025-05-01 18:52:02 +0800

    edit config

--- app/api/config/db_csweb.config.js
+++ app/api/config/db_csweb.config.js
@@ -3,16 +3,16 @@ const knex = require("knex");
 const db = knex({
   client: "mssql",
   connection: {
-    host: "183.81.170.9",
+    server: "183.81.170.9",
     port: 1433,
     user: "user1212",
     password: "wZGAKCXpZUEB",
     database: "NSOweb",
-  },
-  options: {
-    connectTimeout: 40000,
-    encrypt: false,
-    trustServerCertificate: false,
+    options: {
+      encrypt: true,                   // try true first (many servers now require encryption)
+      trustServerCertificate: true,   // skip cert validation for testing
+      connectTimeout: 40000,
+    },
   },
   debug: true,
 });
