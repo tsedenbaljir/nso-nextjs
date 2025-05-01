7f72942ae9b5f6df36128fa5f04bd244baa4f48b
tsedenbaljir <tsedenbaljir@nso.mn>
2025-05-01 14:07:03 +0800

    edit

--- app/api/config/db_csweb.config.js
+++ app/api/config/db_csweb.config.js
@@ -3,7 +3,7 @@ const knex = require("knex");
 const db = knex({
   client: "mssql",
   connection: {
-    host: "183.81.170.9",
+    host: "127.0.0.1",
     port: 1433,
     user: "user1212",
     password: "wZGAKCXpZUEB",
