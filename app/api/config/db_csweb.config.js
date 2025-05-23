ba666fab1b406e5dbeb3300217db2de05a7897cd
tsedenbaljir <tsedenbaljir@nso.mn>
2025-05-23 16:56:09 +0800

    edit

--- app/api/config/db_csweb.config.js
+++ app/api/config/db_csweb.config.js
@@ -1,21 +1,20 @@
 const knex = require("knex");
- 
+
 const db = knex({
   client: "mssql",
   connection: {
-    server: "183.81.170.9",
+    server: "127.0.0.1",
     port: 1433,
     user: "user1212",
     password: "wZGAKCXpZUEB",
     database: "NSOweb",
     options: {
-      encrypt: true,
-      trustServerCertificate: true,
+      encrypt: true,                   // try true first (many servers now require encryption)
+      trustServerCertificate: true,   // skip cert validation for testing
       connectTimeout: 40000,
     },
   },
   debug: true,
 });
- 
-module.exports = { db };
 
+module.exports = { db };
