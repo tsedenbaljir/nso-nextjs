8925084e38afea73b4617fab99f0fa229ff605b0
tsedenbaljir <tsedenbaljire@gmail.com>
2025-08-11 18:06:41 +0800

    ed

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -2,16 +2,11 @@ module.exports = {
   apps: [
     {
       name: "nso.mn",
-      script: "npm",
-      args: "run start", // start скриптээ зөв ажиллуулах
-      exec_mode: "cluster", // ZERO-downtime байлгахын тулд cluster mode ашиглана
-      instances: 2, // 2 instances хэрэгтэй (эсвэл "max")
-      watch: false, // production-д watch хэрэггүй
-      autorestart: true, // апп унасан тохиолдолд дахин эхлэнэ
-      max_memory_restart: "512M", // санах ойг хэтрүүлэхгүй байх
+      script: "npm run start",
+      watch: false,
       env: {
         NODE_ENV: "production",
-        UPLOAD_PATH: "/home/nso/uploads",
+        UPLOAD_PATH: "/home/nso/uploads"
       }
     },
   ],
@@ -22,7 +17,8 @@ module.exports = {
       ref: "origin/main",
       repo: "https://github.com/tsedenbaljir/nso-nextjs.git",
       path: "/home/nso/nso.mn",
-      "post-deploy": "npm ci && npm run build && pm2 reload ecosystem.config.js --env production", // npm install-ийг ci-р сольсон
+      "post-deploy":
+        "npm install --force && npm run build && pm2 reload ecosystem.config.js --env production",
       shallow: true
     },
   },
