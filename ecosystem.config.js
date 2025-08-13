0df6691fd4fa9544713b318d4999365d8c2516b7
tsedenbaljir <tsedenbaljire@gmail.com>
2025-08-13 10:04:33 +0800

    edit

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -3,11 +3,8 @@ module.exports = {
     {
       name: "nso.mn",
       script: "npm run start",
-      watch: true,
-      env: {
-        NODE_ENV: "production",
-        UPLOAD_PATH: "/home/nso/nso.mn/source/public/uploads",
-      }
+      watch: false,
+      env: { NODE_ENV: "production", PORT: 3000 }
     },
   ],
   deploy: {
@@ -23,32 +20,3 @@ module.exports = {
     },
   },
 };
-// module.exports = {
-//   apps: [
-//     {
-//       name: "nso.mn",
-//       script: "npm",
-//       args: "run start", // start скриптээ зөв ажиллуулах
-//       exec_mode: "cluster", // ZERO-downtime байлгахын тулд cluster mode ашиглана
-//       instances: 2, // 2 instances хэрэгтэй (эсвэл "max")
-//       watch: false, // production-д watch хэрэггүй
-//       autorestart: true, // апп унасан тохиолдолд дахин эхлэнэ
-//       max_memory_restart: "512M", // санах ойг хэтрүүлэхгүй байх
-//       env: {
-//         NODE_ENV: "production",
-//         UPLOAD_PATH: "/home/nso/uploads",
-//       }
-//     },
-//   ],
-//   deploy: {
-//     production: {
-//       user: "nso",
-//       host: "183.81.170.9",
-//       ref: "origin/main",
-//       repo: "https://github.com/tsedenbaljir/nso-nextjs.git",
-//       path: "/home/nso/nso.mn",
-//       "post-deploy": "npm ci && npm run build && pm2 reload ecosystem.config.js --env production", // npm install-ийг ci-р сольсон
-//       shallow: true
-//     },
-//   },
-// };
