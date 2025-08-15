037e13dd9a1881e9d73538688fbc649456e2c64b
tsedenbaljir <tsedenbaljire@gmail.com>
2025-08-15 19:11:57 +0800

    edit

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -1,30 +1,20 @@
 module.exports = {
   apps: [
     {
-      name: "nso.mn",
-      cwd: "/home/nso/nso.mn/current",
-      script: "node",
-      args: ".next/standalone/server.js",
-      interpreter: "none",
-      watch: false,
-      env_production: {
-        NODE_ENV: "production",
-        PORT: 3000,
-        HOSTNAME: "0.0.0.0"
-      }
+      name: "nso-new.mn",
+      script: "npm run start",
+      watch: true
     },
   ],
   deploy: {
     production: {
       user: "nso",
       host: "183.81.170.9",
-      ssh_options: "ForwardAgent=yes,StrictHostKeyChecking=no",
+      ssh_options: "StrictHostKeyChecking=no",
       ref: "origin/main",
       repo: "git@github.com:tsedenbaljir/nso-nextjs.git",
-      path: "/home/nso/nso.mn",
-      // Build хийсний дараа standalone-д static + public-ийг хуулна
-      "post-deploy":
-      "bash -lc 'set -e; export NODE_OPTIONS=--max_old_space_size=2048; npm ci || npm install --force; npm run build; mkdir -p .next/standalone/.next; cp -r .next/static .next/standalone/.next/static; [ -d public ] && cp -r public .next/standalone/public || true; pm2 startOrReload ecosystem.config.js --env production'",
+      path: "/home/nso/nso-new.mn",
+      "post-deploy": "npm install --force && npm run build && pm2 reload ecosystem.config.js --env production",
       shallow: true
     },
   },
