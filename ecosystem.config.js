1bf2754dc6f1a87e56150ec36ac395ed31d48714
tsedenbaljir <tsedenbaljire@gmail.com>
2025-08-15 18:19:06 +0800

    edit

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -14,9 +14,7 @@ module.exports = {
       ref: "origin/main",
       repo: "git@github.com:tsedenbaljir/nso-nextjs.git",
       path: "/home/nso/nso.mn",
-      "pre-deploy-local": "echo 'Deploying to production...'",
-      "post-deploy": "cd /home/nso/nso.mn/current && npm install --force && npm run build && pm2 reload ecosystem.config.js --env production",
-      "pre-setup": "echo 'Setting up deployment...'",
+      "post-deploy": "npm install --force && npm run build && pm2 reload ecosystem.config.js --env production",
       shallow: true
     },
   },
