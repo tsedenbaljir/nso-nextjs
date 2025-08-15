23a3d0bed736cd2e290451eb624ce6a89db0dae1
tsedenbaljir <tsedenbaljire@gmail.com>
2025-08-15 21:06:55 +0800

    test

--- app/[lng]/(client)/home/page.js
+++ app/[lng]/(client)/home/page.js
@@ -10,7 +10,7 @@ export default function Home({ params: { lng } }) {
     <Layout lng={lng}>
       <HomeSection indicators={[]} searchData={[]} lng={lng} />
       <Sector lng={lng} />
-      <DisseminationHome lng={lng} />
+      {/* <DisseminationHome lng={lng} /> */}
       <CarouselNews lng={lng} />
       <br/>
     </Layout>
