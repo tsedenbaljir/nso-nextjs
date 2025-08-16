6a4bb7b92ff42bb10fd28df58505076042eb47ba
tsedenbaljir <tsedenbaljire@gmail.com>
2025-08-16 13:21:40 +0800

    edit

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -2,23 +2,27 @@ module.exports = {
   apps: [
     {
       name: "nso-new.mn",
-      cwd: "/home/nso/nso-new.mn/current", // Бүх файлуудын зөв зам
-      script: "npm",  // npm-ийг ашиглах
-      args: "run start",  // командын аргумент
-      interpreter: "none", // Node.js ашиглана
-      watch: true
-    },
+      cwd: "/home/nso/nso-new.mn/current",
+      script: "npm run start",
+      watch: false,
+      env: {
+        NODE_ENV: "production"
+      }
+    }
   ],
+
   deploy: {
     production: {
       user: "nso",
       host: "183.81.170.9",
-      ssh_options: "StrictHostKeyChecking=no",
       ref: "origin/main",
       repo: "git@github.com:tsedenbaljir/nso-nextjs.git",
       path: "/home/nso/nso-new.mn",
-      "post-deploy": "npm install --force && npm run build && pm2 reload ecosystem.config.js --env production",
+      ssh_options: "StrictHostKeyChecking=no",
+      "pre-deploy-local": "",
+      "post-deploy":
+        "npm install --force && npm run build && pm2 reload ecosystem.config.js --env production",
       shallow: true
-    },
-  },
+    }
+  }
 };
