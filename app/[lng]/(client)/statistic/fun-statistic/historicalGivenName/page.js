9ace4211a1728f678d7286cc0c971e188a13d4b6
tsedenbaljir <tsedenbaljire@gmail.com>
2025-11-20 11:35:17 +0800

    edit

--- app/[lng]/(client)/statistic/fun-statistic/historicalGivenName/page.js
+++ app/[lng]/(client)/statistic/fun-statistic/historicalGivenName/page.js
@@ -1,15 +1,63 @@
+// "use client";
+
+// export default function Statistic({ params }) {
+
+//     return (
+//         <>
+//             <div className="nso_container">
+//                 <div className="__detail_body" style={{ width: "100%" }}>
+//                     <div className="__stat_detail_tableau" style={{ backgroundColor: "white", width: "100%" }}>
+//                         <iframe src="https://www2.1212.mn/sonirkholtoi/HistoricalGivenName_new/" height="1200px"
+//                             frameBorder="0" width="100%">
+//                         </iframe>
+//                     </div>
+//                 </div>
+//             </div>
+//         </>
+//     );
+// }
 "use client";
 
-export default function Statistic({ params }) {
+import Link from "next/link";
 
+export default function Statistic({ params }) {
     return (
         <>
             <div className="nso_container">
                 <div className="__detail_body" style={{ width: "100%" }}>
-                    <div className="__stat_detail_tableau" style={{ backgroundColor: "white", width: "100%" }}>
-                        <iframe src="https://www2.1212.mn/sonirkholtoi/HistoricalGivenName_new/" height="1200px"
-                            frameBorder="0" width="100%">
-                        </iframe>
+                    <div
+                        className="__stat_detail_tableau"
+                        style={{
+                            backgroundColor: "white",
+                            width: "100%",
+                            minHeight: "400px",
+                            display: "flex",
+                            flexDirection: "column",
+                            alignItems: "center",
+                            justifyContent: "center",
+                            padding: "24px",
+                            boxSizing: "border-box",
+                        }}
+                    >
+                        <p style={{ marginBottom: "16px", textAlign: "center" }}>
+                            Энэ хуудас засвартай байна.
+                        </p>
+
+                        <Link
+                            href="https://www.1212.mn/mn/statistic/fun-statistic/home"
+                            target="_blank"
+                            rel="noopener noreferrer"
+                            style={{
+                                padding: "10px 20px",
+                                borderRadius: "6px",
+                                border: "1px solid #1976d2",
+                                textDecoration: "none",
+                                fontWeight: 500,
+                                display: "inline-block",
+                            }}
+                        >
+                            Буцах
+                        </Link>
                     </div>
                 </div>
             </div>
