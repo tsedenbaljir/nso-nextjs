88e0a795be96b94f285f2aceba373db24d102ac4
tsedenbaljir <tsedenbaljir@nso.mn>
2026-08-05 11:12:39 +0800

    edit css

--- app/[lng]/(client)/statistic/fun-statistic/home/page.js
+++ app/[lng]/(client)/statistic/fun-statistic/home/page.js
@@ -65,6 +65,28 @@ export default function Statistic(props) {
 
           <div
             className="flex flex-col items-center cursor-pointer text-justify text-[--font-size15] hover:scale-[1.01] transition-transform"
+            onClick={() => router.push(`${base}/commodity-price`)}
+            style={{ width: "300px" }}
+          >
+            <img
+              className="w-[250px] h-[250px] rounded-[20px] object-cover"
+              src="/images/sonirkholtoi/price.png"
+              alt=""
+            />
+            <br />
+            <label className="text-center text-[--font-size20] font-semibold hover:text-[--accent]">
+              {lng === "mn"
+                ? "Гол нэрийн барааны үнийн харьцуулалт"
+                : "Commodity price comparison"}
+            </label>
+            <span className="desc">
+              {lng === "mn"
+                ? "Гол нэрийн бараа, бүтээгдэхүүний үнийн өөрчлөлт, харьцуулалт."
+                : "Comparison of commodity prices and trends."}
+            </span>
+          </div>
+          <div
+            className="flex flex-col items-center cursor-pointer text-justify text-[--font-size15] hover:scale-[1.01] transition-transform"
             onClick={() => router.push(`${base}/human`)}
             style={{ width: "300px" }}
           >
@@ -183,28 +205,6 @@ export default function Statistic(props) {
               {lng === "mn"
                 ? "1935 оноос 2024 он хүртэлх жилийн эцсийн хүн амын тоо"
                 : ""}
-            </span>
-          </div>
-          <div
-            className="flex flex-col items-center cursor-pointer text-justify text-[--font-size15] hover:scale-[1.01] transition-transform"
-            onClick={() => router.push(`${base}/commodity-price`)}
-            style={{ width: "300px" }}
-          >
-            <img
-              className="w-[250px] h-[250px] rounded-[20px] object-cover"
-              src="https://gateway.1212.mn/services/fms/api/public/download/0/GBsYBy-h_BNhG1VcOD2-F39lYi_LXw1CIdnfwE_t.jpg"
-              alt=""
-            />
-            <br />
-            <label className="text-center text-[--font-size20] font-semibold hover:text-[--accent]">
-              {lng === "mn"
-                ? "Гол нэрийн барааны үнийн тооцоолол"
-                : "Main commodity price calculator"}
-            </label>
-            <span className="desc">
-              {lng === "mn"
-                ? "Гол нэрийн бараа, бүтээгдэхүүний үнийн өөрчлөлт, тооцоолол."
-                : "Average prices and trends for main selected commodities."}
             </span>
           </div>
           {/* <div
